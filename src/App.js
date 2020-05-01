929fffc39f3592c8c7b8fb6b2d78e9d72f78d14e
umbrelluck <yarko.umbrelluck@gmail.com>
2020-05-01 11:24:12 +0300

    base

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import './content/css/main.css'
-
-import News from './content/news';
-import Trends from './content/trends';
-import Header from './content/header';
-import Footer from './content/footer'
-
-var page = { "main": false };
 
 function App() {
   return (
-    <div className="container">
-      <Header></Header>
-      <Trends page={page}></Trends>
-      <News page={page} />
-      <Footer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
