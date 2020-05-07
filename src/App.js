d1996886fc20c48c2af28193a8ce221dd2621a3e
umbrelluck <yarko.umbrelluck@gmail.com>
2020-05-07 16:46:20 +0300

    removed comments

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import './content/css/main.css'
 
 import Body from './content/body'
-// import Header from './content/header';
 import Footer from './content/footer'
 
 import {BrowserRouter} from 'react-router-dom';
@@ -12,7 +11,6 @@ function App() {
   return (
     <BrowserRouter>
       <div className="container">
-        {/* <Header></Header> */}
         <Body></Body>
         <Footer />
       </div>
